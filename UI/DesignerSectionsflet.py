06d4a7c6ffede55ab3f8f495232cb25ac071afa1
Nafay Ahmed <nafayahmed@Nafays-Mac-mini.lan>
2023-10-02 19:42:15 +0300

    changed made

--- UI/DesignerSectionsflet.py
+++ UI/DesignerSectionsflet.py
@@ -9,14 +9,16 @@ class DesignerSection(ft.UserControl):
 
     def accept_draggable(self, e: ft.DragTargetAcceptEvent):
         src = self._get_control_name()
-        e.control.content.content = ft.Container(
-            bgcolor=ft.colors.BLUE_700,
-            width=50,
-            height=50,
-            # border=ft.border.all(3, color=ft.colors.CYAN_300),
-            # border_radius=ft.border_radius.all(10),
-            # margin=ft.margin.only(left=100, top=25),
-        )
+        e.control.content.controls = [
+            ft.Container(
+                bgcolor=ft.colors.BLUE_700,
+                width=50,
+                height=50,
+                border=ft.border.all(3, color=ft.colors.CYAN_300),
+                border_radius=ft.border_radius.all(10),
+                margin=ft.margin.only(left=100, top=25),
+            )
+        ]
         e.control.update()
 
     def build(self):
@@ -24,16 +26,16 @@ class DesignerSection(ft.UserControl):
             group="widget",
             on_accept=self.accept_draggable,
             content=ft.Stack(
+                width=1200,
+                height=1000,
                 controls=[
-                    ft.Container(
-                        width=1200,
-                        height=1000,
-                        border=ft.border.all(3, color=ft.colors.CYAN_300),
-                        border_radius=ft.border_radius.all(10),
-                        bgcolor=ft.colors.BLACK12,
-                        margin=ft.margin.only(left=100, top=25),
-                    ),
-                ]
+                    # ft.Container(
+                    #     border=ft.border.all(3, color=ft.colors.CYAN_300),
+                    #     border_radius=ft.border_radius.all(10),
+                    #     bgcolor=ft.colors.BLACK12,
+                    #     margin=ft.margin.only(left=100, top=25),
+                    # ),
+                ],
             ),
         )
 
