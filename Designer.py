c6948ab683e4c797ec74a98c6aa4ab09181acd76
A.Nafay <opmusab844@gmail.com>
2023-10-03 18:50:09 +0300

    Initial

--- Designer.py
+++ Designer.py
@@ -1,4 +1,5 @@
 import flet as ft
+from flet import Container
 from Parser.Parserengine import Parser
 
 from UI.ToolbarItem import ToolbarItem
@@ -15,22 +16,31 @@ class DesignerSection(ft.UserControl):
         # self.parser.AddWidget()
         getcrlname = e.control
         ctrlname = e.page.get_control(e.src_id)
-        print(ctrlname.content.content.value)
-        print(ctrlname.content.height)
+        name = ctrlname.content.content.value
+
+        # cont = ft.Container(bgcolor=ft.colors.BLUE_900, width=200, height=200)
+
+        object = globals()[name]
+        object = object(bgcolor=ft.colors.BLUE_900, width=100, height=200)
+        e.control.content.controls[0].content = ft.Container(content=[object])
+
+        e.control.update()
+        # print(ctrlname.content.height)
 
     def build(self):
         self.DesignerSection1 = ft.DragTarget(
             group="widget",
             on_accept=self.accept_draggable,
             content=ft.Stack(
-                width=1200,
-                height=1000,
                 controls=[
                     ft.Container(
                         border=ft.border.all(3, color=ft.colors.WHITE24),
                         border_radius=ft.border_radius.all(10),
                         bgcolor=ft.colors.BLACK12,
                         margin=ft.margin.only(left=100, top=25),
+                        width=1200,
+                        height=1000,
+                        # alignment=ft.alignment.center,
                     ),
                 ],
             ),
--- example.py
+++ example.py
@@ -19,34 +19,30 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    def on_pan_update1(e: ft.DragUpdateEvent):
-        c.top = max(0, c.top + e.delta_y)
-        c.left = max(0, c.left + e.delta_x)
-        c.update()
-
-    def on_pan_update2(e: ft.DragUpdateEvent):
-        e.control.top = max(0, e.control.top + e.delta_y)
-        e.control.left = max(0, e.control.left + e.delta_x)
+    def on_pan_update(e: ft.DragUpdateEvent):
+        e.control.width = max(50, (e.control.width or 0) + e.delta_x)
+        e.control.height = max(50, (e.control.height or 0) + e.delta_y)
         e.control.update()
 
     gd = ft.GestureDetector(
         mouse_cursor=ft.MouseCursor.MOVE,
-        drag_interval=10,
-        on_pan_update=on_pan_update1,
+        on_vertical_drag_update=on_pan_update,
+        left=50,
+        top=25,
+        content=ft.Container(
+            bgcolor=ft.colors.BLUE, width=50, height=50, border_radius=10
+        ),
     )
 
-    c = ft.Container(gd, bgcolor=ft.colors.AMBER, width=50, height=50, left=0, top=0)
-
-    gd1 = ft.GestureDetector(
-        mouse_cursor=ft.MouseCursor.RESIZE_DOWN,
-        drag_interval=10,
-        on_vertical_drag_update=on_pan_update2,
-        left=100,
-        top=100,
-        content=ft.Container(bgcolor=ft.colors.BLUE, width=50, height=50),
+    page.add(
+        ft.Stack(
+            height=page.window_height,
+            controls=[gd],
+            expand=True,
+            width=page.window_width,
+        )
     )
-
-    page.add(ft.Stack([c, gd1], width=1000, height=500))
+    page.padding = 0
 
 
 ft.app(target=main)
